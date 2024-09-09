fff52b7ee8508e50b42c466476e818b533f0a18f
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-09-09 22:02:02 +0530

    Design UI using Blade components and display mock data

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                black: "#060606",
+            },
+            fontFamily: {
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
+            },
+            fontSize: {
+                "2xs": ".625rem", // 10px
+            },
+        },
     },
     plugins: [],
 };
